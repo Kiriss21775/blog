fcfea012e478fd83698b44570e94078cbf8e2ba1
Kiriss21775 <7korol@gmail.com>
2022-10-19 14:51:14 +0300

    posts: CRUD, partially without 'content'

--- resources/views/Admin/includes/sidebar.blade.php
+++ resources/views/Admin/includes/sidebar.blade.php
@@ -22,6 +22,14 @@
                     </p>
                 </a>
             </li>
+            <li class="nav-item">
+                <a href="{{route('admin.post.index')}}" class="nav-link">
+                    <i class="nav-icon far fa-clipboard "></i>
+                    <p>
+                        Посты
+                    </p>
+                </a>
+            </li>
         </ul>
     </div>
     <!-- /.sidebar -->
