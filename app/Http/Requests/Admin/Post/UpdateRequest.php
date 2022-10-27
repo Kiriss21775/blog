d29f2f0eabc863c228cc5c6e3114deff56c818a0
Kiriss21775 <7korol@gmail.com>
2022-10-27 16:10:54 +0300

    add Servise, fix error message

--- app/Http/Requests/Admin/Post/UpdateRequest.php
+++ app/Http/Requests/Admin/Post/UpdateRequest.php
@@ -33,4 +33,19 @@ class UpdateRequest extends FormRequest
             'tag_ids.*' => 'nullable|integer|exists:tags,id',
         ];
     }
+    public function messages()
+    {
+        return [
+            'title.required' => 'Это поле обязательно для заполнения',
+            'title.string' => 'Данные должны соответсвовать строчному типу',
+            'preview_image.required' => 'Это поле обязательно для заполнения',
+            'preview_image.file' => 'Необходимо выбрать файл',
+            'main_image.required' => 'Это поле обязательно для заполнения',
+            'main_image.file' => 'Необходимо выбрать файл',
+            'category_id.required' => 'Это поле обязательно для заполнения',
+            'category_id.integer' => "Id категории должен быть числом",
+            'category_id.exists' => "Id категории должен быть в базе данных",
+            'tag_ids.array' => 'Необходимо отправить массив данных',
+        ];
+    }
 }
--- resources/views/Admin/post/edit.blade.php
+++ resources/views/Admin/post/edit.blade.php
@@ -35,7 +35,7 @@
                             <div class="form-group" class="w-25">
                                 <input type="text" class="form-control" name="title" placeholder="Название поста" value="{{$post->title}}">
                                 @error('title')
-                                <div class="text-danger">Пустое поле</div>
+                                <div class="text-danger">{{$message}}</div>
                                 @enderror
                             </div>
                             <div class="form-group">
@@ -43,7 +43,7 @@
                                     {{$post->content}}
                                 </textarea>
                                 @error('content')
-                                <div class="text-danger">Пустое поле</div>
+                                <div class="text-danger">{{$message}}</div>
                                 @enderror
                             </div>
                             <div class="form-group w-50">
@@ -61,7 +61,7 @@
                                     </div>
                                 </div>
                                 @error('preview_image')
-                                <div class="text-danger">Пустое поле</div>
+                                <div class="text-danger">{{$message}}</div>
                                 @enderror
                             </div>
                             <div class="form-group w-50">
@@ -79,7 +79,7 @@
                                     </div>
                                 </div>
                                 @error('main_image')
-                                <div class="text-danger">Пустое поле</div>
+                                <div class="text-danger">{{$message}}</div>
                                 @enderror
                             </div>
                             <div class="form-group w-50">
